1699048efb4af0505950ab438d9ee9e5a7fa16cf
edgars61 <edgarsantana.62a@gmail.com>
2020-10-11 10:19:34 -0500

    added search function to application

--- trading/views.py
+++ trading/views.py
@@ -16,49 +16,57 @@ import json
 
 
 def home(request):
-    try:
-        # For Python 3.0 and later
-          from urllib.request import urlopen
-    except ImportError:
-        # Fall back to Python 2's urllib2
-          from urllib2 import urlopen
-          
-    def get_jsonparsed_data(url):
-        """
-        Receive the content of ``url``, parse it as JSON and return the object.
+    if request.method =='POST':
+        ticker = request.POST['ticker']
+        try:
+            # For Python 3.0 and later
+            from urllib.request import urlopen
+        except ImportError:
+            # Fall back to Python 2's urllib2
+            from urllib2 import urlopen
+            
+        def get_jsonparsed_data(url):
+            """
+            Receive the content of ``url``, parse it as JSON and return the object.
 
-        Parameters
-        ----------
-        url : str
+            Parameters
+            ----------
+            url : str
 
-        Returns
-        -------
-        dict
-        """
-        response = urlopen(url)
-        data = response.read().decode("utf-8")
-        return json.loads(data)
+            Returns
+            -------
+            dict
+            """
+            response = urlopen(url)
+            data = response.read().decode("utf-8")
+            return json.loads(data)
 
-    url = ("https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=demo")
-    json_response = get_jsonparsed_data(url)
-   
-    if  json_response:
-        extracted = json_response[0]
-        ticker = extracted["symbol"]
-        df= get_data(ticker, start_date="12/04/2009", end_date="12/04/2019", index_as_date = True, interval="1wk")
-        name = extracted["name"]
-        df['close'].plot()
-        plt.figure(figsize=(10,10))
-        plt.plot(df.index, df['close'])
-        plt.xlabel("date")
-        plt.ylabel("$ price")
-        plt.title(search+ " Stock Price 1/1/17 - 8/1/19")
-        plt.savefig("trading/static/trading/foo.png")
+        url = ("https://financialmodelingprep.com/api/v3/profile/"+ticker+"?apikey=07eb8824ce1236dcbba7f02dca51447f")
+        json_response = get_jsonparsed_data(url)
         
-        return render(request,'home.html',{'extracted':extracted})
-        
+        if  json_response:
+            extracted = json_response[0]
+            ticker = extracted["symbol"]
+            df= get_data(ticker, start_date="12/04/2009", end_date="12/04/2019", index_as_date = True, interval="1wk")
+            df['close'].plot()
+            plt.figure(figsize=(10,10))
+            plt.plot(df.index, df['close'])
+            plt.xlabel("date")
+            plt.ylabel("$ price")
+            plt.title(ticker+ " Stock Price 1/1/17 - 8/1/19")
+            plt.savefig("trading/static/trading/foo.png")
+            
+            
+            
+        else:
+            extracted = "There was an error with your ticker please try again."
     else:
-        return HttpResponse('<h1></h1>')
+        extracted = "gay porn"
+       
+    return render(request,'home_1.html',{'extracted':extracted})
+        
+        
+    
 
 
     
