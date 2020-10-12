501d3b8654f290d6a0b75c2d6bb6aed5fa4c9f06
edgars61 <edgarsantana.62a@gmail.com>
2020-10-11 22:49:15 -0500

    created models

--- trading/admin.py
+++ trading/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import User, Stock, Transaction, PF_Value_Daily
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=['name']
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display=['ticker']
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display=['transaction_type','transaction_value','stock_sold','date']
+
+@admin.register(PF_Value_Daily)
+class DailyValueAdmin(admin.ModelAdmin):
+    list_display=['value','date_ending']
--- trading/models.py
+++ trading/models.py
@@ -2,9 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    balance = models.FloatField(default=1000.00)
+    transactions = models.ManyToManyField('Transaction',blank=True)
+    stocks = models.ManyToManyField('Stock',blank=True)
+    pf_value = models.ManyToManyField('PF_Value_Daily',blank = True)
+    def __self__(self):
+       return self.name
 
 class Stock(models.Model):
-    name = models.CharField(max_length=10)
-    quantity = models.IntegerField()
-    purchase_value = models.IntegerField()
+    ticker = models.CharField(max_length=10)
+
+class Transaction(models.Model):
+    TRANSACTION_CHOICES =[('P','Purchase'),('S','Sell')]
+    transaction_type = models.CharField(max_length=1, choices=TRANSACTION_CHOICES)
+    transaction_value = models.FloatField(default=0.0)
+    stock_sold = models.CharField(max_length=10)
+    date = models.DateTimeField() 
     
+
+class PF_Value_Daily(models.Model):
+    value = models.FloatField(default=0.0)
+    date_ending = models.DateTimeField()
